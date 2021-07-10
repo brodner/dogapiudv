0abb1200c2599719e092fcee6f5727518136f126
brodner <brodner.rocael.m.a@gmail.com>
2021-07-10 11:06:34 -0600

    Add: nodemon, axios / add API DOGAPI

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const PORT = 3001
 app.use(express.static('public'))
 
@@ -8,6 +9,18 @@ app.get('/', (request, response) => {
   console.log(request.ips)
   console.log(request.originalUrl)
   response.location('./public')
+})
+
+app.get('/dogs',(request,res)=>{
+  axios.get('https://dog.ceo/api/breeds/list/all')
+  .then(response => res.status(201).send(response.data))
+})
+
+app.get('/dogs/:breed',(request,res)=>{
+  const { breed } = request.params
+  axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+  .then(response => res.status(201).send(response.data))
+  .catch(err => res.status(404).json({"message":err.message}) )
 })
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
